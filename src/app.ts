35bf4a679ad7c03373bd8038793b04d71889bf8f
Qus <qus1225@gmail.com>
2019-02-06 00:23:40 +0900

    Router 모듈화

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,11 @@ import dotenv from "dotenv";
 import path from "path";
 import bodyParser from "body-parser";
 import mustacheExpress from "mustache-express";
-
+import router from "./router";
 
 
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv.config({ path: ".env.production" });
-
-
-// Controllers (route handlers)
-import * as homeController from "./controllers/home";
 
 
 // API keys and Passport configuration
@@ -34,23 +30,17 @@ const app = express();
 
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 // Register '.mustache' extension with The Mustache Express
-app.engine('mst', mustacheExpress());
+app.engine("mst", mustacheExpress());
 app.set("view engine", "mst");
 app.set("views", path.join(__dirname, "../views"));
+app.use(router);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
-
-
-/**
- * Primary app routes.
- */
-app.get("/", homeController.index);
-
 
 /**
  * API examples routes.
